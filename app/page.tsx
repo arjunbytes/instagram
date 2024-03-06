498c9779b8f3728455d982d4951228b39d073408
arjunbytes <nagarchunan@gmail.com>
2024-03-06 21:55:22 +0530

    Shadcn UI CSS Update

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h5 className="text-3xl">Instagram</h5>
+    <main className="flex min-h-screen bg-white flex-col items-center justify-between p-24">
+      <h5 className="text-3xl text-black">Instagram</h5>
     </main>
   );
 }
